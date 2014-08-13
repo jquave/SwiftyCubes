f2ccedb6771c818b949ad1a68910197a5b5ca379
Jameson Quave <jquave@gmail.com>
2014-08-13 17:03:48 -0500

    Added more comments

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 
-### SwiftyCube
+### SwiftyCubes
 
-A demo of working with programmatic UIViews, using UIDynamics, and handling touches manually. Also bouncy cubes. Your welcome, world.
+A demo of working with programmatic UIViews, using UIDynamics, and handling touches manually.
+Also bouncy cubes.
+
+There's no tutorial or anything yet, so I added a bunch of comments.
+100% of the logic is in ViewController.swift, so just look at that. The rest is your basic template.
+Enjoy!
 
 ![SwiftyCubes](https://raw.githubusercontent.com/jquave/SwiftyCubes/master/SwiftyCubes.gif)
 
--- SwiftyCube/ViewController.swift
+++ SwiftyCube/ViewController.swift
@@ -64,25 +64,40 @@ class ViewController: UIViewController {
     }
     
     
-    
+    /// Creates a cube object and adds it to the view, as well as the physicsObjects array.
     func addCube(x : CGFloat, y: CGFloat, size cubeSize : CGFloat) {
         
         if(showCubes) {
+            // Instantiate a new UIView
             var cube = UIView()
+            
+            // Set some properties of it's border (blue, 1 point wide, with a 25% corner radius.)
+            // We have to do this on the UIView's backing layer, since it's just not available in UIView
             cube.layer.borderColor = UIColor.blueColor().CGColor
             cube.layer.borderWidth = 1
             cube.layer.cornerRadius = cubeSize/4.0
+            
+            // Set the x and y position, as well as the size of the view by setting the frame
             cube.frame = CGRectMake(x, y, cubeSize, cubeSize)
+            
+            // Set the background color to be colorPattern, which is define above as a UIColor containing a tiled image
             cube.backgroundColor = colorPattern
+            
+            // Set the tag of this view to the constant CUBE_TAG, so we can identify it later as a physics cube
             cube.tag = CUBE_TAG
             
             
+            // Add the cube to the main view
             self.view.addSubview(cube)
             
+            
+            // Add the cube to the list of physicsObjects (a property of ViewController)
             physicsObjects.append(cube)
         }
         else {
         
+            // This does nearly all the same things as above, but for a UILabel
+            // This is only visible if showCubes is set to false.
             var txtLabel = UILabel(frame: CGRectMake(x, y, cubeSize, cubeSize))
             txtLabel.text = "Hello!"
             txtLabel.userInteractionEnabled = true
@@ -98,26 +113,54 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    
+    // ************************************
+    // MARK: Touch callback methods
+    // ************************************
+    
+    /// iOS calls this when the user places a finger (our clicks the mouse) on the screen
     override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        
+        // See if we can get the view that was clicked by accessing the view property in touches.anyObject()
         if let tappedView = touches.anyObject().view {
+            
+            // If a view was found, check and see if it's tag is CUBE_TAG
+            // Remember earlier? We set the tag of all views to CUBE_TAG?
+            // That was so we can check it here.
             if(tappedView.tag == CUBE_TAG) {
+                // Okay, this is a legit cube, store this view as the "selectedView" property
                 selectedView = tappedView
             }
         }
     }
     
+    /// iOS calls this when the user has already had a finger on the screen, and now they're moving it around
     override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!) {
-        //selectedView?.center = touches.anyObject().locationInView(self.view)
+        
+        // activeSnapper is a property we created for the Snap behavior we want when the user drags around a cube
+        // Since the user is now moving around their finger, we remove any existing snap behavior and create a fresh one
         if(activeSnapper != nil) {
             animator?.removeBehavior(activeSnapper)
         }
+        
+        // Since the user is dragging around their finger, we want to act upon the selectedView.
+        // But, it might be nil since they could just be dragging around the background.
+        // Imagine a user trying to grab a cube, but they have terrible aim.
         if(selectedView != nil) {
+            // Create a new Snap behavior for the selectedView, and set the point to be whatever the location
+            // of the user's touch is, inside of the main view (the whole screen in this case)
             activeSnapper = UISnapBehavior(item: selectedView, snapToPoint: touches.anyObject().locationInView(self.view))
+            
+            // Upping the damping makes the snap behavior more elastic and loose. It's a personal preference.
             activeSnapper?.damping = 2
+            
+            // Now add the behavior to our animator so it kicks in.
             animator?.addBehavior(activeSnapper)
         }
     }
     
+    /// iOS calls this when the user slides their finger clear off the screen
     override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
         selectedView = nil
         if(activeSnapper != nil) {
@@ -125,6 +168,8 @@ class ViewController: UIViewController {
         }
     }
 
+    
+    /// iOS calls this when the user lifts their finger off the screen
     override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
         selectedView = nil
         if(activeSnapper != nil) {
@@ -132,6 +177,46 @@ class ViewController: UIViewController {
         }
     }
 
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
