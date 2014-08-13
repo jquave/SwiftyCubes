64acd7ccbb4be57952b6bf3e18e10ace1c959986
Jameson Quave <jquave@gmail.com>
2014-08-13 17:12:28 -0500

    Even more comments

--- SwiftyCube/ViewController.swift
+++ SwiftyCube/ViewController.swift
@@ -19,48 +19,73 @@ class ViewController: UIViewController {
     // Tag for cube views
     let CUBE_TAG = 100
     
+    
+    // These properties are explained in detail as they're used
     var animator : UIDynamicAnimator?
     var gravity : UIGravityBehavior?
     var collisionBehavior : UICollisionBehavior?
+    var activeSnapper : UISnapBehavior?
     var physicsObjects = [UIView]()
     
-    var activeSnapper : UISnapBehavior?
-    
+    /// The "texture" on our cubes.
     let colorPattern = UIColor(patternImage: UIImage(named: "checkered"))
     
+    /// The currently selected cube
     var selectedView : UIView? = nil
     
-    /// This is called when the view loads.
+    /// This is called when the view loads. Why? Because iOS just does that in View Controllers.
     override func viewDidLoad() {
-        super.viewDidLoad()
         
-        var screenWidth = CGFloat(UIScreen.mainScreen().bounds.size.width)
-        var cubeSize = CGFloat(screenWidth) / CGFloat(sqrt(Float(numCubes)))
+        // Get the width of the iPhone screen. UIScreen.mainScreen() pretty much always gives you the only screen.
+        // Occasionally (like if on AirPlay) there are two screens, but it's not an issue here.
+        // We use the bounds to get the width of the screen and set the size of our cubes to be
+        // whatever that is divided by the square root of the number of cubes. This gives us a
+        // size of cube we know we can fit numCubes of on to the screen.
+        let screenWidth = CGFloat(UIScreen.mainScreen().bounds.size.width)
+        let cubeSize = CGFloat(screenWidth) / CGFloat(sqrt(Float(numCubes)))
         
+        // Just a few pixels of spacing to go between the cubes
         let spacing = CGFloat(2.0)
         
+        // Nested loop, goes through and creates a bunch of cubes by stepping through, adding
+        // the appropriate spacing and widths as needed.
         for (var x : CGFloat = spacing; x<screenWidth; x+=cubeSize + spacing) {
             for (var y : CGFloat = spacing; y<screenWidth; y+=cubeSize + spacing) {
+                
+                // Calls our addCube method above with our x and y locations, and the cubeSize we figured out earlier.
                 addCube(x, y: y, size: cubeSize)
             }
         }
         
+        
+        // An animator is needed for any of this to work
         animator = UIDynamicAnimator(referenceView: self.view)
         
+        // Create an empty gravity behavior, we need this for the cubes to fall
         gravity = UIGravityBehavior()
         
-        
-        
+        // Create a collision behavior, with all the physicsObjects that ended up in our physicsObjects array
+        // They get added inside of the addCube() function
         collisionBehavior = UICollisionBehavior(items: physicsObjects)
+        
+        // Set the references bounds in to boundaries.
+        // Basically this means just take the area that the cube takes up as a View, and turn that in to
+        // collision boundaries. This includes the screen edges.
         collisionBehavior?.translatesReferenceBoundsIntoBoundary = true
         
+        
+        // Now loops through all the physicsObjects and add them to the gravity behavior.
         for physicsObject in self.physicsObjects {
             gravity?.addItem(physicsObject)
         }
         
+        // Finally, add the gravity and collision behaviors on to the animator. iOS takes over from here.
         animator!.addBehavior(gravity)
         animator!.addBehavior(collisionBehavior)
         
+        
+        // Call the superclass method for viewDidLoad()
+        super.viewDidLoad()
     }
     
     
@@ -162,6 +187,7 @@ class ViewController: UIViewController {
     
     /// iOS calls this when the user slides their finger clear off the screen
     override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        // Since they aren't touching the screen any more, just nil out the selectedView and remove any snapping behaviors
         selectedView = nil
         if(activeSnapper != nil) {
             animator?.removeBehavior(activeSnapper)
@@ -171,6 +197,8 @@ class ViewController: UIViewController {
     
     /// iOS calls this when the user lifts their finger off the screen
     override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
+        // Same as above...
+        // "Since they aren't touching the screen any more, just nil out the selectedView and remove any snapping behaviors"
         selectedView = nil
         if(activeSnapper != nil) {
             animator?.removeBehavior(activeSnapper)
