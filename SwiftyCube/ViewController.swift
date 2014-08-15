0f5f9885e5be7d10dd4b16a6628503a0ec41fe9c
Jameson Quave <jquave@gmail.com>
2014-08-15 01:20:41 -0500

    Use anti-aliasing, and also why not add an animated hue-shifting background?

--- SwiftyCube/ViewController.swift
+++ SwiftyCube/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -26,10 +27,17 @@ class ViewController: UIViewController {
     
     /// For each cube that is being dragged, we have a snapper. There can be multiple snappers because multitouch!
     var snappers = [UIView: UISnapBehavior]()
-
+    
+    // The color of the background in the very beginning
+    var bgColor = UIColor(hue: 1, saturation: 0.5, brightness: 0.9, alpha: 1.0)
+    // This is used to measure the time, which is used to select a color for the background
+    var frameTime = 0.0
     
     /// This is called when the view loads. Why? Because iOS just does that in View Controllers.
     override func viewDidLoad() {
+        
+        // Create a timer that fires every 60th of a second to call the function changeColor(), its declared below...
+        NSTimer.scheduledTimerWithTimeInterval(1.0/60.0, target: self, selector: "changeColor", userInfo: nil, repeats: true)
         
         // Get the width of the iPhone screen. UIScreen.mainScreen() pretty much always gives you the only screen.
         // Occasionally (like if on AirPlay) there are two screens, but it's not an issue here.
@@ -87,6 +95,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    
+    /// Update the color of the background by shifting the hue
+    func changeColor() {
+        // How much time has passed since the last frame?
+        var dt = CACurrentMediaTime() - frameTime
+        
+        var hue = CGFloat(0.0)
+        var saturation = CGFloat(0.0)
+        var brightness = CGFloat(0.0)
+        var alpha = CGFloat(0.0)
+        if(bgColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)) {
+            hue -= CGFloat(dt*0.1)
+            bgColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+            self.view.backgroundColor = bgColor
+        }
+        frameTime = CACurrentMediaTime()
+    }
+    
     // This method is called whenever one of the gesture recognizers created in the addCube method detects a drag gesture.
     func onDrag(recognizer: UIPanGestureRecognizer) {
         // The recognizer's view is the view we added the gesture recognizer to - that is the cube view!
@@ -111,7 +137,7 @@ class ViewController: UIViewController {
             snappers[cubeView] = snapper
                 
             // Upping the damping makes the snap behavior more elastic and loose. It's a personal preference.
-            snapper.damping = 2
+            snapper.damping = 3
             
             // Now add the behavior to our animator so it kicks in.
             animator.addBehavior(snapper)
@@ -121,43 +147,33 @@ class ViewController: UIViewController {
     /// Creates a cube object.
     func createCube(x : CGFloat, y: CGFloat, size cubeSize : CGFloat) -> UIView {
         
-        if showCubes {
-            // Instantiate a new UIView
-            var cube = UIView()
-            
-            // Set some properties of it's border (blue, 1 point wide, with a 25% corner radius.)
-            // We have to do this on the UIView's backing layer, since it's just not available in UIView
-            cube.layer.borderColor = UIColor.blueColor().CGColor
-            cube.layer.borderWidth = 1
-            cube.layer.cornerRadius = cubeSize/4.0
-            
-            // Set the x and y position, as well as the size of the view by setting the frame
-            cube.frame = CGRectMake(x, y, cubeSize, cubeSize)
-            
-            // Set the background color to be colorPattern, which is define above as a UIColor containing a tiled image
-            cube.backgroundColor = colorPattern
-            
-            // We add a pan-gesture recognizer to drag a cube, for each cube separately.
-            // "Pan"ning is basically equivalent to dragging in our case.
-            // The gesture recognizer will call our onDrag: method whenever the finger moves.
-            cube.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "onDrag:"))
-
-            return cube
-        }
-        else {
+        // Instantiate a new UIView
+        var cube = UIView()
         
-            // This does nearly all the same things as above, but for a UILabel
-            // This is only visible if showCubes is set to false.
-            var txtLabel = UILabel(frame: CGRectMake(x, y, cubeSize, cubeSize))
-            txtLabel.text = "Hello!"
-            txtLabel.userInteractionEnabled = true
-            txtLabel.layer.borderColor = UIColor(red: 0, green: 0.5, blue: 0, alpha: 1).CGColor
-            txtLabel.layer.borderWidth = 1
-            txtLabel.textColor = UIColor.blueColor()
-            txtLabel.layer.cornerRadius = cubeSize/4.0
-            txtLabel.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "onDrag:"))
+        // Set some properties of it's border (blue, 1 point wide, with a 25% corner radius.)
+        // We have to do this on the UIView's backing layer, since it's just not available in UIView
+        cube.layer.borderColor = UIColor(red: 0.0, green: 0.1, blue: 0.6, alpha: 1).CGColor
+        cube.layer.borderWidth = 1
+        cube.layer.cornerRadius = cubeSize/4.0
+        
+        // Set the x and y position, as well as the size of the view by setting the frame
+        cube.frame = CGRectMake(x, y, cubeSize, cubeSize)
+        
+        // Set the background color to be colorPattern, which is define above as a UIColor containing a tiled image
+        cube.backgroundColor = colorPattern
+        
+        // We add a pan-gesture recognizer to drag a cube, for each cube separately.
+        // "Pan"ning is basically equivalent to dragging in our case.
+        // The gesture recognizer will call our onDrag: method whenever the finger moves.
+        cube.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "onDrag:"))
+        
+        
+        // Turn on anti-aliasing
+        // - OR -
+        // Make jaggedy edges more smoothy
+        cube.layer.allowsEdgeAntialiasing = true
 
-            return txtLabel
-        }
+        return cube
+        
     }
 }
